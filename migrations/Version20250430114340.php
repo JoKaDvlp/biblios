be7c973de489438c310bbd5cc5a27785309e3de3
JoKaDvlp <jonathan.karmann@gmail.com>
2025-04-30 15:01:59 +0200

    Dependance to orm-fixtures zenstruck/foundry

---
+++ migrations/Version20250430114340.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250430114340 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE book DROP created_by_id
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user ADD last_connected_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE book ADD created_by_id INT NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user DROP last_connected_at
+        SQL);
+    }
+}
